2da3ff708b6af17bab3a22ff1d7804524b89b8dd
monsterarth <abp9141@gmail.com>
2025-06-20 15:12:26 -0300

    imgfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // A linha 'unoptimized: true' foi removida.
-    // Agora adicionamos o remotePatterns para o domínio do Vercel Blob.
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'public.blob.vercel-storage.com',
+        hostname: '**.public.blob.vercel-storage.com',
         port: '',
-        pathname: '/:path*', // Permite qualquer caminho dentro do hostname
+        pathname: '/**',
       },
     ],
   },
